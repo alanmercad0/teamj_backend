2eace08f7687963a451c90f11cfbb83414c51917
dlaboy <diego.laboy1@upr.edu>
2024-10-28 18:58:27 -0400

    preliminar check for less than 4 minutes

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app.third_party.shazam.shazam_class import ShazamAPIClass
 from app.third_party.musicai.musicai_class import MusicAIClass,download_youtube_video_as_mp3
 from flask import request
 from pprint import pprint
+from mutagen.mp3 import MP3
 
 import asyncio
 bp = Blueprint('main', __name__)
@@ -20,9 +21,23 @@ async def process_song():
     shazam_instance = ShazamAPIClass()
     musicai_instance = MusicAIClass()
     mp3= download_youtube_video_as_mp3(ytb_url,'./app/third_party/shazam')
-    recognize_song = await shazam_instance.recognize_track(mp3)
-    new_job = await musicai_instance.create_job('new job',mp3)
-    result = [recognize_song,new_job]
-    print(f"process result",result)
+    print(f"MP3 file: {mp3}")
+
+    checking_mp3 = MP3(mp3)
+    print(f"Checking MP3 object: {mp3}")
+
+    length_in_seconds = checking_mp3.info.length
+    minutes, seconds = divmod(length_in_seconds, 60)
+    print(f"Length of the MP3 file: {int(minutes)} minutes and {int(seconds)} seconds")
+
+    if minutes <= 4:
+        recognize_song = await shazam_instance.recognize_track(mp3)
+        new_job = await musicai_instance.create_job('new job',mp3)
+        result = [recognize_song,new_job]
+        print(f"process result",result)
+    else:
+        result = f"Invalid Song: Song must be less than 4:00 minutes. Song Length {minutes}:{seconds}"
+        print(result)
+        
     return result
 
